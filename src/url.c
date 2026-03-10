983887b3434d707520e28415bf70e3253f582cd7
Vlad Voloshchuk <vladuslav0811@gmail.com>
2026-03-10 16:49:19 +0200

    add URL parsing module

---
+++ src/url.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include "url.h"
+
+int url_parse(const char *url_str, Url *url)
+{
+    memset(url, 0, sizeof(Url));
+
+    const char *url_ptr;
+
+    url_ptr = strstr(url_str, "://");
+    if (!url_ptr)
+    {
+        fprintf(stderr, "Invalid URL\n");
+        return -1;
+    }
+
+    int scheme_len = url_ptr - url_str;
+    strncpy(url->scheme, url_str, scheme_len);
+
+    url_ptr += 3;
+
+    const char *host_start = url_ptr;
+
+    while (*url_ptr && *url_ptr != ':' && *url_ptr != '/')
+        url_ptr++;
+
+    int host_len = url_ptr - host_start;
+    strncpy(url->host, host_start, host_len);
+
+    if (*url_ptr == ':')
+    {
+        url_ptr++;
+
+        url->port = atoi(url_ptr);
+
+        while (*url_ptr && *url_ptr != '/')
+            url_ptr++;
+    }
+    else
+    {
+        url->port = 80;
+    }
+
+    if (*url_ptr == '/')
+    {
+        strcpy(url->path, url_ptr);
+    }
+    else
+    {
+        strcpy(url->path, "/");
+    }
+
+    return 0;
+}
---
+++ src/url.h
@@ -0,0 +1,14 @@
+#ifndef URL_PARSER_H
+#define URL_PARSER_H
+
+typedef struct
+{
+    char scheme[16];
+    char host[256];
+    int port;
+    char path[1024];
+} Url;
+
+int url_parse(const char *url_str, Url *url);
+
+#endif
