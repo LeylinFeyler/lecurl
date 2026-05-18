94462bf3997bcd5d17bec12d284e52654a49b3d4
Vlad Voloshchuk <vladuslav0811@gmail.com>
2026-05-18 21:32:18 +0300

    extend CLI options and request configuration

--- src/cli.h
+++ src/cli.h
@@ -1,14 +1,35 @@
 #ifndef CLI_H
 #define CLI_H
 
-typedef struct {
+#define MAX_HEADERS 32
+
+typedef struct
+{
     char *url;
     char *output_file;
     int head_only;
     int include_headers;
+
+    char *method;
+    char *body;
+    int timeout;
+    int follow_redirects;
+    int retry_count;
+    char *auth;
+
+    char *headers[MAX_HEADERS];
+    int header_count;
+
 } CliOptions;
 
-int cli_parse(int argc, char **argv, CliOptions *opts);
-void cli_help(const char *prog);
+int cli_parse(
+        int argc,
+        char **argv,
+        CliOptions *opts
+);
+
+void cli_help(
+        const char *prog
+);
 
 #endif
