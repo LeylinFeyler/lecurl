568f411ccc76ee9a98db65ffaf09792fe9a4c41c
Vlad Voloshchuk <vladuslav0811@gmail.com>
2026-05-19 14:21:46 +0300

    unify options structure and add extended request flags

--- src/cli.c
+++ src/cli.c
@@ -2,7 +2,16 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <getopt.h>
+#include <string.h>
 #include "cli.h"
+
+static struct option long_options[]=
+{
+    {"connect-timeout",required_argument,0,1000},
+    {"retry",required_argument,0,1001},
+
+    {0,0,0,0}
+};
 
 static void init_opts(CliOptions *opts)
 {
@@ -10,6 +19,14 @@ static void init_opts(CliOptions *opts)
     opts->output_file = NULL;
     opts->head_only = 0;
     opts->include_headers = 0;
+        memset(
+        opts,
+        0,
+        sizeof(*opts)
+    );
+    opts->method="GET";
+    opts->timeout=5;
+    opts->retry_count=0;
 }
 
 int cli_parse(int argc, char **argv, CliOptions *opts)
@@ -18,7 +35,12 @@ int cli_parse(int argc, char **argv, CliOptions *opts)
 
     int opt;
 
-    while ((opt = getopt(argc, argv, "o:Iih")) != -1)
+    while((opt=getopt_long(
+            argc,
+            argv,
+            "o:IiH:X:d:u:Lh",
+            long_options,
+            NULL))!=-1)
     {
         switch (opt)
         {
@@ -37,6 +59,49 @@ int cli_parse(int argc, char **argv, CliOptions *opts)
             case 'h':
                 cli_help(argv[0]);
                 exit(0);
+
+            case 'X':
+                opts->method=optarg;
+                break;
+
+            case 'd':
+                opts->body=optarg;
+                break;
+
+            case 'H':
+                if(opts->header_count>=MAX_HEADERS)
+                {
+                    fprintf(
+                        stderr,
+                        "too many headers\n"
+                    );
+
+                    return -1;
+                }
+
+                opts->headers[
+                    opts->header_count++
+                ]=optarg;
+
+                break;
+
+            case 'u':
+                opts->auth=optarg;
+                break;
+
+            case 'L':
+                opts->follow_redirects=1;
+                break;
+
+            case 1000:
+                opts->timeout=
+                    atoi(optarg);
+                break;
+
+            case 1001:
+                opts->retry_count=
+                    atoi(optarg);
+                break;
 
             default:
                 cli_help(argv[0]);
@@ -63,4 +128,11 @@ void cli_help(const char *prog)
     printf("  -I          HEAD request\n");
     printf("  -i          include headers\n");
     printf("  -h          help\n");
+    printf("  -X METHOD      request method\n");
+    printf("  -d DATA        request body\n");
+    printf("  -H HEADER      custom header\n");
+    printf("  -u USER:PASS   basic auth\n");
+    printf("  -L             follow redirects\n");
+    printf("  --retry N      retry count\n");
+    printf("  --connect-timeout SEC\n");    
 }
--- src/cli.h
+++ src/cli.h
@@ -7,29 +7,25 @@ typedef struct
 {
     char *url;
     char *output_file;
-    int head_only;
-    int include_headers;
 
     char *method;
     char *body;
+
+    char *auth;
+
+    int head_only;
+    int include_headers;
+
     int timeout;
     int follow_redirects;
     int retry_count;
-    char *auth;
 
     char *headers[MAX_HEADERS];
     int header_count;
 
 } CliOptions;
 
-int cli_parse(
-        int argc,
-        char **argv,
-        CliOptions *opts
-);
-
-void cli_help(
-        const char *prog
-);
+int cli_parse(int argc, char **argv, CliOptions *opts);
+void cli_help(const char *prog);
 
 #endif
